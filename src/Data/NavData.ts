166656603e02b8651676add18cba838a314e0f52
tdeepak383 <tdeepak383@gmail.com>
2024-10-18 17:33:03 +0530

    files update

---
+++ src/Data/NavData.ts
@@ -0,0 +1,32 @@
+const NavItems = [
+    {
+        text: "Home",
+        href: "/",
+        style: "Normal",
+        subtext: [""]
+    },
+
+    {
+        text: "Blog",
+        href: "/blog/index.html",
+        style: "Dropdown",
+        subtext: ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+    },
+    {
+        text: "About Us",
+        href: "/about/index.html",
+        style: "Dropdown",
+        subtext: ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+    },
+
+    {
+        text: "Contact Us",
+        href: "/contact/index.html",
+        style: "Normal",
+        subtext: [""]
+    },
+
+];
+
+
+export default NavItems;
